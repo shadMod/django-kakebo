30e101430617b167a38ad2db20b8e07fe788b239
mattia allegro <mattia.allegro@yahoo.it>
2023-08-13 00:45:20 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="django_kakebo",
-    version="0.0.1",
+    version="0.0.5",
     author="shadMod",
     author_email="support@shadmod.it",
     description="A simple site made with django and exploiting the kakebo method",
