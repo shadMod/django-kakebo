eedf6a129dd50c983c6e93570c9e1d260ad5af9a
mattia allegro <mattia.allegro@yahoo.it>
2023-08-13 14:51:18 +0200

    added five_reason in context to dynamic flowchart

--- django_kakebo/src/django_kakebo/views.py
+++ django_kakebo/src/django_kakebo/views.py
@@ -3,3 +3,34 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = "basic/home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["five_reason"] = [
+            {
+                "name": "ORDINE",
+                "description": "Lorem ipsium",
+                "favicon": '<i class="bi bi-list-ul"></i>',
+            },
+            {
+                "name": "CONTROLLO",
+                "description": "Lorem ipsium",
+                "favicon": '<i class="bi bi-controller"></i>',
+            },
+            {
+                "name": "RISPARMIO",
+                "description": "Lorem ipsium",
+                "favicon": '<i class="bi bi-piggy-bank"></i>',
+            },
+            {
+                "name": "AUTODISCIPLINA",
+                "description": "Lorem ipsium",
+                "favicon": '<i class="bi bi-cup-hot"></i>',
+            },
+            {
+                "name": "SERENITA'",
+                "description": "Lorem ipsium",
+                "favicon": '<i class="bi bi-emoji-laughing"></i>',
+            },
+        ]
+        return context
