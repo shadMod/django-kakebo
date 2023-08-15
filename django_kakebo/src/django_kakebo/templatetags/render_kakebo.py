6714f52521336b15d1e19faf4884815313dc5bdd
mattia allegro <mattia.allegro@yahoo.it>
2023-08-15 14:05:02 +0200

    - added get_data_byobj() to check and get determinate cell (to get 'desc' and 'value' key) - added possibility to get determinate KakeboWeek() - added modal to set description and value input

--- django_kakebo/src/django_kakebo/templatetags/render_kakebo.py
+++ django_kakebo/src/django_kakebo/templatetags/render_kakebo.py
@@ -1,11 +1,28 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django_kakebo.constants import colors as list_colors
+from django_kakebo.models import KakeboWeek
 
 register = template.Library()
 
 
+def get_data_byobj(kakebo: dict, color: str, row: int, column: int):
+    key_color = int(list_colors.index(color))
+
+    if not any(
+            kakebo
+            and kakebo.get(f"{key_color}", "")
+            and kakebo.get(f"{key_color}", "").get(f"{column}", "")
+            and kakebo.get(f"{key_color}", "").get(f"{column}", "").get(f"{row}", "")
+    ):
+        return None
+
+    return kakebo[f"{key_color}"][f"{column}"][f"{row}"]
+
+
 @register.simple_tag
 def white_space_table():
+    # a simple body to render a white space
     html = """
     <tbody>
         <tr>
@@ -20,7 +37,20 @@ def white_space_table():
 
 @register.filter(is_safe=True)
 @register.simple_tag
-def render_table(color: str, row: int = 7, name: str = None):
+def render_table(color: str = None, row: int = 7, name: str = None, kakebo: str = None):
+    """
+    Render table with a determinate color and row
+    """
+    if color is None:
+        color = "orange"
+
+    # get kakebo table week from db
+    user, year, week = kakebo.split('-')
+    obj = KakeboWeek.objects.get(
+        # user=user,
+        year=year, week=week,
+    ).data_row
+
     html = "<tbody>"
     for i in range(row):
         html += "<tr>"
@@ -32,18 +62,82 @@ def render_table(color: str, row: int = 7, name: str = None):
                 </h5>
             </td>
             """
-        for cell in range(7):
+        for clm in range(7):  # 7 days in one week
             if i == 0:
                 html += f'<td class="bt-8-{color} bx-{color} px-15">'
-            elif i == 6:
+            elif i == row - 1:
                 html += f'<td class="bb-{color} bx-{color} px-15">'
             else:
                 html += f'<td class="bx-{color} px-15">'
+
+            data_ = get_data_byobj(obj, color, i, clm)
+
+            tag_name_modal = f"tag_name_{color}_{clm}_{i}"
             html += f"""
-                <p class="bb-dashed-{color} mb-2">
-                    <input>
+                <p class="bb-dashed-{color} mb-2 text-end">
+                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#{tag_name_modal}">
+            """
+            if data_ is not None:
+                html += f"""
+                    {data_['desc']} - {data_['value']}
+                """
+            html += f"""
+                        <i class="bi bi-pencil-square"></i>
+                    </button>
                 </p>
             </td>
+            
+            <div class="modal fade" id="{tag_name_modal}" tabindex="-1"
+                aria-labelledby="{tag_name_modal}_label" aria-hidden="true">
+                <div class="modal-dialog modal-lg modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="{tag_name_modal}_label">
+                                Descrizione e valore
+                            </h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                                
+                            <div class="form-group row">
+                                <div class="col-sm-10">
+                                    <label class="col-form-label">
+                                        Descrizione:
+                                    </label>
+                                    <textarea class="form-control" id="id_{tag_name_modal}_desc" 
+                                        name="{tag_name_modal}_desc" rows="4" cols="50"
+                                        
+            """
+
+            html += 'placeholder="description">'
+            html += data_["desc"] if data_ is not None else ""
+            html += "</textarea>"
+
+            html += f"""
+                                </div>
+                                <div class="col-sm-2">
+                                    <label class="col-form-label">
+                                        Valore:
+                                    </label>
+                                    <input type="number" class="form-control" id="id_{tag_name_modal}_value"
+                                        name="{tag_name_modal}_value" style="border: 1px solid grey"
+            """
+
+            html += f"value={data_['value']}>" if data_ is not None else ">"
+
+            html += """
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">
+                                Salva
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             """
         html += "</tr>"
     html += "</tbody>"
