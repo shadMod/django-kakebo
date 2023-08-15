c7c806795e92bb45def131aa19ffeff638f79be0
mattia allegro <mattia.allegro@yahoo.it>
2023-08-15 14:00:51 +0200

    - replaced KakeboWeek() with KakeboWeekFormView() with form_valid to make JSON - added dispatch() to get or create KakeboWeek in DB when the page is loaded - added get_time() to have the first day of week according to the week number and year chosen

--- django_kakebo/src/django_kakebo/views.py
+++ django_kakebo/src/django_kakebo/views.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
 
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, FormView
+
+from .models import KakeboWeek
+from .forms import KakeboWeekForm
+from .constants import colors
 
 
 class Index(TemplateView):
@@ -38,25 +44,78 @@ class Index(TemplateView):
         return context
 
 
-class KakeboWeek(TemplateView):
+class KakeboWeekFormView(FormView):
+    form_class = KakeboWeekForm
     template_name = "basic/kakebo/week.html"
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_anonymous:
+            pass
+
+        self.obj, _ = KakeboWeek.objects.get_or_create(
+            # user=self.kwargs["username"],
+            year=self.kwargs["year"],
+            week=self.kwargs["week"],
+        )
+        self.year = kwargs['year']
+        self.week = kwargs['week']
+        self.time_ = self.get_time
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        cd = form.cleaned_data
+
+        data = {}
+        for j in range(4):
+            data[j] = {}
+            color = colors[j]
+            for clm, day in enumerate(self.get_list_day(self.time_)):
+                data[j][clm] = {}
+                for i in range(9):
+                    tag_name = f"tag_name_{color}_{clm}_{i}"
+                    desc = cd.get(f"{tag_name}_desc", None)
+                    value = cd.get(f"{tag_name}_value", None)
+                    if desc or value:
+                        data[j][clm][i] = {"desc": desc, "value": value}
+
+        self.obj.data_row = data
+        self.obj.save()
+        return self.get_success_url()
+
+    def get_success_url(self):
+        return HttpResponseRedirect(
+            reverse_lazy(
+                "kakebo-week", kwargs={
+                    "username": self.kwargs["username"],
+                    "year": self.kwargs["year"],
+                    "week": self.kwargs["week"],
+                },
+            )
+        )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['cell_'] = self.get_list_day("07082023")
+
+        list_tr = []
+        for i, lenrows in enumerate([9, 7, 7, 7]):
+            list_tr.append((colors[i], lenrows))
+        context['list_tr'] = list_tr
+
+        context['cell_'] = self.get_list_day(self.time_)
         context['row_'] = [i for i in range(7)]
+        context['key_kakebo'] = f'{self.kwargs["username"]}-{self.kwargs["year"]}-{self.kwargs["week"]}'
         return context
 
-    def get_list_day(self, today: str = None) -> list:
-        if today is None:
-            today = datetime.now()
-        else:
-            today = datetime.strptime(today, "%d%m%Y")
+    @property
+    def get_time(self) -> datetime:
+        date_w = f"{self.year}-{self.week}-1"
+        return datetime.strptime(date_w, "%Y-%W-%w")
 
+    @staticmethod
+    def get_list_day(today: datetime) -> list:
         list_days = []
         for i in range(7):
             data__ = f"{today.strftime('%A')} - {today.strftime('%d')}"
             list_days.append(data__)
             today += timedelta(days=1)
-
         return list_days
