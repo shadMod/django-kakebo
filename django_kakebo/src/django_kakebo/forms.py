52dabd23e6c7149b39d97ce3161cc44afae7b4e7
mattia allegro <mattia.allegro@yahoo.it>
2023-08-15 14:02:03 +0200

    added forms with KakeboWeekForm()

---
+++ django_kakebo/src/django_kakebo/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .constants import colors
+
+
+class KakeboWeekForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(KakeboWeekForm, self).__init__(*args, **kwargs)
+
+        for color in colors:
+            for row in range(9):  # casual max value (number)
+                for clm in range(7):  # 7 days in one week
+                    tag_name = f"tag_name_{color}_{clm}_{row}"
+                    self.fields[f"{tag_name}_desc"] = forms.CharField(
+                        max_length=200, required=False
+                    )
+                    self.fields[f"{tag_name}_value"] = forms.IntegerField(
+                        required=False
+                    )
