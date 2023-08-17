9376ba8fe221f6403e1d133a48acca77ab00447f
mattia allegro <mattia.allegro@yahoo.it>
2023-08-17 11:57:12 +0200

    rm python3.7; added user app in packages; added all install_requires (non-updated requirements); set version 0.0.11

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import os
 import glob
 import setuptools
 
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+
+def get_install_requires() -> list:
+    """Returns requirements.txt parsed to a list"""
+    fname = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    targets = []
+    if os.path.exists(fname):
+        with open(fname, 'r') as f:
+            targets = f.read().splitlines()
+    return targets
+
 
 setuptools.setup(
     name="django_kakebo",
@@ -25,15 +37,16 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     include_package_data=True,
     packages=[
         "django_kakebo", "django_kakebo.src.django_kakebo", "django_kakebo.src.django_kakebo.migrations",
-        "django_kakebo.src.django_kakebo.templatetags", "django_kakebo.src.django_kakebo.views"
+        "django_kakebo.src.django_kakebo.templatetags", "django_kakebo.src.django_kakebo.views",
+        "django_kakebo.src.user"
     ],
     data_files=[
         (
@@ -44,6 +57,11 @@ setuptools.setup(
             "themes",
             [fn for fn in glob.iglob("django_kakebo/src/themes/**/*", recursive=True) if "." in fn],
         ),
+        (
+            "static_user",
+            [fn for fn in glob.iglob("django_kakebo/src/user/static/**/*", recursive=True) if "." in fn],
+        ),
     ],
+    install_requires=get_install_requires(),
     python_requires=">=3.7",
 )
