1456874d8c7739c6408e51421c5be5bcdfb82b75
mattia allegro <mattia.allegro@yahoo.it>
2023-08-17 11:39:57 +0200

    added LoginRequiredMixin() in all views

--- django_kakebo/src/django_kakebo/views/settings.py
+++ django_kakebo/src/django_kakebo/views/settings.py
@@ -1,13 +1,14 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class KakeboDashboardSetting(TemplateView):
+class KakeboDashboardSetting(LoginRequiredMixin, TemplateView):
     template_name = "settings/dashboard.html"
 
 
-class KakeboSettings(TemplateView):
+class KakeboSettings(LoginRequiredMixin, TemplateView):
     template_name = "settings/settings.html"
 
 
-class KakeboThemes(TemplateView):
+class KakeboThemes(LoginRequiredMixin, TemplateView):
     template_name = "settings/themes.html"
