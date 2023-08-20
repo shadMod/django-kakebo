cef47d73420002049a4f036b16e529b96b7dc30a
mattia allegro <mattia.allegro@yahoo.it>
2023-08-20 18:27:59 +0200

    added KakeboMonth admin management

--- django_kakebo/src/django_kakebo/admin.py
+++ django_kakebo/src/django_kakebo/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import KakeboWeek, KakeboWeekTable
+from .models import KakeboMonth, KakeboWeek, KakeboWeekTable
+
+
+@admin.register(KakeboMonth)
+class KakeboMonthAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(KakeboWeek)
