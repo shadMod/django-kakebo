77b2284ec048e5ed85d7f16623b638e3fecd7ac7
mattia allegro <mattia.allegro@yahoo.it>
2023-09-02 13:23:42 +0200

    quick fix urls path; set version 0.1.4

--- django_kakebo/urls.py
+++ django_kakebo/urls.py
@@ -1,1 +1,6 @@
-from .src.django_kakebo.urls import urlpatterns
+from .src.django_kakebo.urls import urlpatterns as urlpatterns_kakebo
+from .src.user.urls import urlpatterns as urlpatterns_user
+
+urlpatterns = []
+urlpatterns.extend(urlpatterns_kakebo)
+urlpatterns.extend(urlpatterns_user)
