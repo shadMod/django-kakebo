2c9eae6edecdddd619c5cc7e7aa7a80910bba994
mattia allegro <mattia.allegro@yahoo.it>
2025-03-06 08:58:13 +0100

    sort files

--- django_kakebo/urls.py
+++ django_kakebo/urls.py
@@ -1,6 +1,27 @@
-from .src.django_kakebo.urls import urlpatterns as urlpatterns_kakebo
-from .src.user.urls import urlpatterns as urlpatterns_user
+from django.urls import path
 
-urlpatterns = []
-urlpatterns.extend(urlpatterns_kakebo)
-urlpatterns.extend(urlpatterns_user)
+from .views.kakebo import (
+    EndOfMonthBalanceSheetFormView,
+    IndexKakeboView,
+    KakeboWeekFormView,
+    SelectYearWeekFormView,
+)
+
+urlpatterns = [
+    path("", IndexKakeboView.as_view(), name="kakebo-home"),
+    path("calendar/", SelectYearWeekFormView.as_view(), name="kakebo-calendar"),
+    path(
+        "calendar/<int:year>/", SelectYearWeekFormView.as_view(), name="kakebo-calendar"
+    ),
+    path(
+        "calendar/<int:year>/<int:month>/",
+        SelectYearWeekFormView.as_view(),
+        name="kakebo-calendar",
+    ),
+    path("<int:year>/<int:week>/", KakeboWeekFormView.as_view(), name="kakebo-week"),
+    path(
+        "calendar/<int:year>/<int:month>/report/",
+        EndOfMonthBalanceSheetFormView.as_view(),
+        name="kakebo-balance",
+    ),
+]
